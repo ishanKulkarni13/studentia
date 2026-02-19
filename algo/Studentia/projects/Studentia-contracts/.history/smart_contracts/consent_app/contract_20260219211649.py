bb49a194c9ebcdd2cbe59c8e1f0a8a9219ddccd7
ishankulkarni13 <ishankulkarni13@gmail.com>
2026-02-19 22:01:05 +0530

    eat:consent: revert box storage attempt due to compiler limits

---
+++ algo/Studentia/projects/Studentia-contracts/.history/smart_contracts/consent_app/contract_20260219211649.py
@@ -0,0 +1,30 @@
+from algopy import ARC4Contract, BoxMap, String, UInt64
+from algopy.arc4 import abimethod
+
+
+class ConsentApp(ARC4Contract):
+    # Persist consent status per key in boxes: 1 = granted, 0 = revoked
+    consents = BoxMap(String, UInt64)
+
+    def _make_key(self, student_id: String, receiver_group: String, data_group: String) -> String:
+        return student_id + ":" + receiver_group + ":" + data_group
+
+    @abimethod()
+    def grant_consent(self, student_id: String, receiver_group: String, data_group: String) -> String:
+        key = self._make_key(student_id, receiver_group, data_group)
+        self.consents[key] = UInt64(1)
+        return "GRANTED:" + key
+
+    @abimethod()
+    def revoke_consent(self, student_id: String, receiver_group: String, data_group: String) -> String:
+        key = self._make_key(student_id, receiver_group, data_group)
+        self.consents[key] = UInt64(0)
+        return "REVOKED:" + key
+
+    @abimethod(readonly=True)
+    def get_consent(self, student_id: String, receiver_group: String, data_group: String) -> String:
+        key = self._make_key(student_id, receiver_group, data_group)
+        status, exists = self.consents.maybe(key)
+        if not exists:
+            return "NONE:" + key
+        return ("GRANTED:" if status == UInt64(1) else "REVOKED:") + key
